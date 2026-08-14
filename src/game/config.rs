577e783bd0033e5527fea136989701fe6db3232e
Ashwagandhae <julianlianbauer@gmail.com>
2026-08-15 01:46:52 +1200

    fix config and build wasm

--- src/game/config.rs
+++ src/game/config.rs
@@ -16,10 +16,10 @@ pub struct GameConfig {
 
 #[cfg(target_arch = "wasm32")]
 /// Game configuration
+#[derive(Default)]
 pub struct GameConfig {
     /// whether or not cheats are on
     pub(crate) cheat: bool,
     /// level to start the game on
     pub(crate) level: Option<usize>,
 }
-
